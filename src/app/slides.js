cfbe5315c2c4ab879e03975040342b733f4e7d2c
779g <adrian.villa@779.mx>
2020-06-26 15:05:00 +0000

    [Auto] Compile and deploy from cli

--- src/app/slides.js
+++ src/app/slides.js
@@ -1,4 +1,7 @@
+import * as React from 'react'
+
 export default [
+
   /*
   {
     id              :'mnt-1',
@@ -13,7 +16,7 @@ export default [
     source          :'/a01.jpg',
     alt             :'mnt',
     caption         :'Karelia, 2018',
-    wrapperClassName:'x-black'
+    wrapperClassName:'x-black b-x'
         
   },
   {
@@ -21,7 +24,7 @@ export default [
     source          :'/a02.jpg',
     alt             :'mnt',
     caption         :'Karelia, 2018',
-    wrapperClassName:'x-green'
+    wrapperClassName:'x-green b-x'
         
   },
   {
@@ -29,7 +32,7 @@ export default [
     source          :'/a03.jpg',
     alt             :'mnt',
     caption         :'Teheran, 2016',
-    wrapperClassName:'x-background'
+    wrapperClassName:'x-background b-x'
         
   },
   {
@@ -37,23 +40,23 @@ export default [
     source          :'/a04.jpg',
     alt             :'',
     caption         :'Cuba, 2016',
-    wrapperClassName:'x-olive'
+    wrapperClassName:'x-olive b-x'
         
   },
   {
     id              :'vfc',
     source          :'/a05.jpg',
     alt             :'',
-    caption         :'South of France, 2017',
-    wrapperClassName:'x-white'
+    caption         :'France, 2017',
+    wrapperClassName:'x-white b-x'
         
   },
   {
     id              :'mty',
     source          :'/a06.jpg',
     alt             :'',
-    caption         :'Monterrey, 2016',
-    wrapperClassName:'x-white'
+    caption         :'Mexico, 2016',
+    wrapperClassName:'x-white b-x'
         
   },
   {
@@ -61,7 +64,7 @@ export default [
     source          :'/a07.jpg',
     alt             :'',
     caption         :'Oaxaca, 2016',
-    wrapperClassName:'x-grey'
+    wrapperClassName:'x-grey b-x'
         
   },
   {
@@ -69,6 +72,66 @@ export default [
     source          :'/a08.jpg',
     alt             :'',
     caption         :'Paris, 2009',
+    wrapperClassName:'x-white b-x'
+        
+  },
+  {
+    id              :'tree',
+    source          :'/a09.jpg',
+    alt             :'',
+    caption         :'Oaxaca, 2016',
+    wrapperClassName:'x-olive b-x'
+        
+  },
+  {
+    id              :'cannes',
+    source          :'/a10.jpg',
+    alt             :'',
+    caption         :'France, 2013',
+    wrapperClassName:'x-black b-x'
+        
+  },
+  {
+    id :'explanation',
+    jsx:<>
+      <p><strong>Hello. I have taken these pictures over the years.</strong></p>
+      <p>These pictures are all raw scans of Polaroid film prints and haven't been digitally edited. Particular colors or effects come from analog manipulation and the uniqueness of every film pack.</p>
+    </>,
+    wrapperClassName:'x-white'
+        
+  },
+  {
+    id :'contact',
+    jsx:<>
+      <p>
+        <strong>
+        Contact :
+          <a
+            href="mailto:alexis.zorba@779.mx"
+            className="c-on-x yi"
+            onClick={e => e.stopPropagation()}
+          >
+          alexis.zorba@779.mx
+          </a>
+        </strong>
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong>
+          Source code :
+          <a
+            href="https://github.com/779g/779.mx"
+            className="c-on-x yi"
+            target="_blank"
+            onClick={e => e.stopPropagation()}
+          >
+            github.com/779g/779.mx
+          </a>
+        </strong>
+      </p>
+      <p>&nbsp;</p>
+      <p>Not interested, I just want to see more pictures</p>
+    </>,
     wrapperClassName:'x-white'
         
   }
